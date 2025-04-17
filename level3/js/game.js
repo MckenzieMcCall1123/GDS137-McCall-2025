49423b6fa8eda4228d1125f58d4156e2a72e4a77
MckenzieMcCall1123 <msmccall@email.neit.edu>
2025-04-17 09:44:55 -0400

    two paddles

---
+++ level3/js/game.js
@@ -0,0 +1,148 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+var player;
+var player2;
+
+	//Set Up the Canvas
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	//Instantiate the Player
+	player = new GameObject(25, canvas.height/2);
+	player.width = 25;
+	player.height = 150;
+
+	player2 = new GameObject(25, canvas.height/2);
+	player2.width = 25;
+	player2.height = 150;
+	player2.x = 1000;
+	player2.y = 400;
+	player2.color = "blue";
+
+	ball = new GameObject();
+	ball.vy = 0;
+	ball.vx = 10;
+	ball.width = 50;
+	//Set the Animation Timer
+	timer = setInterval(animate, interval);
+
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	// Player 1
+	if(w && player.y > canvas.height/2 - 325)
+	{
+		console.log("Moving Up");
+		player.y += -5;
+	}
+	if(s && player.y < canvas.height/2 + 325)
+	{
+		console.log("Moving Down");
+		player.y += 5;
+	}
+
+	// Player 2
+	if(up && player2.y > canvas.height/2 - 325)
+	{
+		console.log("Moving Up");
+		player2.y += -5;
+	}
+	if(down && player2.y < canvas.height/2 + 325)
+	{
+		console.log("Moving Down");
+		player2.y += 5;
+	}
+
+	//Update the Screen
+	player.drawRect();
+	player2.drawRect();
+
+	ball.drawCircle();
+	ball.move();
+	
+
+	if(ball.x > canvas.width + -50)
+		{
+			ball.vx = -ball.vx;
+			ball.color = randomColor();
+		}
+	
+	if(ball.x < 0 + 50)
+		{
+			ball.x = 500;
+			ball.y = 500;
+			ball.vx = -ball.vx;
+			ball.color = randomColor();
+		}
+
+
+	if(ball.y > canvas.height + -50 || ball.y < 0 + 50)
+		{
+			ball.vy = -ball.vy;
+			ball.color = randomColor();
+		}
+
+
+	
+		// Paddle section detection mechanics
+	if(ball.hitTestObject(player))
+		{
+		ball.x = player.x + player.width /2 + ball.width /2
+			 //ball hits top
+			 if(ball.y > player.y + player.height/6)
+			 {
+				ball.vy = 5;
+			 }
+			 //ball hit bottom
+			 else if (ball.y < player.y - player.height/6)
+			 {
+				ball.vy = -5;
+			 }
+			 //ball hits middle
+			 else
+			 {
+				ball.vy = 0;
+			 }
+			 ball.vx = -ball.vx;
+		}
+
+		if(ball.hitTestObject(player2))
+			{
+			ball.x = player2.x - player2.width /2 - ball.width /2
+				 //ball hits top
+				 if(ball.y > player2.y + player2.height/6)
+				 {
+					ball.vy = 5;
+				 }
+				 //ball hit bottom
+				 else if (ball.y < player2.y - player2.height/6)
+				 {
+					ball.vy = -5;
+				 }
+				 //ball hits middle
+				 else
+				 {
+					ball.vy = -0;
+				 }
+				 ball.vx = -ball.vx;
+			}
+
+	player2.drawRect();
+	
+}
+
+function randomColor(){
+	var r = Math.round(Math.random()*255)
+	var b = Math.round(Math.random()*255)
+	var g = Math.round(Math.random()*255)
+
+	return `rgb(${r}, ${g}, ${b})`
+}
---
+++ level3/js/week2_collision.js
@@ -0,0 +1,94 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+var player;
+var player2;
+
+//This is used to stop the player from moving through obstacles.
+var prevX;
+
+	//Set Up the Canvas
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	//Instantiate the Player
+	player = new GameObject();
+	player2 = new GameObject();
+
+	player.x = 100;
+	player2.x = 500;
+	player2.y = 300;
+
+
+
+	//Set the Animation Timer
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	
+	//Move the Player to the right
+	if(d)
+	{
+		//console.log("Moving Right");
+		player.x += 2;
+	}
+	
+	if(a)
+	{
+		//console.log("Moving Right");
+		player.x += -2;
+	}
+	
+	
+	//Check Collisions
+	
+	//Demonstrates Accuracy of Bounding Box Collision
+	if(lBlock1.hitTestObject(player))
+	{
+		//change color
+		lBlock1.color = "yellow";
+	}
+	else
+	{
+		lBlock1.color = "#00ff00";
+	}
+	
+	//Shows Bounding Boxes
+	if(lBlock2.hitTestObject(player))
+	{
+		//draw bounding boxes
+		context.strokeRect(lBlock2.x- lBlock2.width/2, lBlock2.y - lBlock2.height/2, lBlock2.width, lBlock2.height)
+		context.strokeRect(player.x- player.width/2, player.y - player.height/2, player.width, player.height)
+	}
+	
+	//Demonstrates how often collisions take place
+	if(rBlock1.hitTestObject(player))
+	{
+		console.log("colliding");
+	}
+	
+	//Impede movement
+	if(rBlock2.hitTestObject(player))
+	{
+		player.x = prevX;
+		console.log("colliding");
+	}
+	else
+	{
+		prevX = player.x;
+	}
+	
+	//Update the Screen
+	player.drawCircle();
+
+
+}
+
